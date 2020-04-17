94d3dcb61807ff3ab2c507d3e1a939feaf528717
Erick Almeida <erickingles@gmail.com>
2020-04-16 23:50:58 -0300

    Adiciona Receitas

--- CookingGame.playground/Contents.swift
+++ CookingGame.playground/Contents.swift
@@ -11,7 +11,7 @@ class StartViewController: UIViewController {
         
         let backgroundImage = UIImage(named: "Images/Interface/cover.png")!
         let background = UIImageView(image: backgroundImage)
-        background.frame = CGRect(x: 0, y: 0, width: 1024, height: 1366)
+        background.frame = CGRect(x: 0, y: 0, width: 1024, height: 768)
         
         let startButtonImage = UIImage(named: "Images/Interface/book.png")!
         let startButton = UIButton()
@@ -24,18 +24,99 @@ class StartViewController: UIViewController {
         self.view = view
     }
     @IBAction func goToCookbook(){
-        navigationController?.show(cookbook, sender: nil)
+        navigationController?.show(kitchen, sender: nil)
         navigationController?.navigationBar.isHidden = true
     }
 }
 
 class CookbookViewController: UIViewController {
-    
+    override func loadView() {
+        navigationController?.navigationBar.isHidden = true
+        
+        let view = UIView()
+        view.frame = CGRect(x: 0, y: 0, width: 1024, height: 768)
+        view.backgroundColor = #colorLiteral(red: 0.7843137255, green: 0.9568627451, blue: 0.9568627451, alpha: 1)
+        
+        let backgroundImage = UIImage(named: "Images/Interface/recipes_page.png")!
+        let background = UIImageView(image: backgroundImage)
+        background.frame = CGRect(x: 0, y: 0, width: 1024, height: 768)
+                
+        view.addSubview(background)
+        self.view = view
+    }
+}
+
+class KitchenViewController: UIViewController {
+    override func loadView() {
+        navigationController?.navigationBar.isHidden = true
+        
+        let view = UIView()
+        view.frame = CGRect(x: 0, y: 0, width: 1024, height: 768)
+        view.backgroundColor = #colorLiteral(red: 0.7843137255, green: 0.9568627451, blue: 0.9568627451, alpha: 1)
+        
+        let backgroundImage = UIImage(named: "Images/Interface/kitchen.png")!
+        let background = UIImageView(image: backgroundImage)
+        background.frame = CGRect(x: 0, y: 0, width: 1024, height: 768)
+        
+        let foodShelfImage = UIImage(named: "Images/Interface/food_shelf.png")
+        let foodShelf = UIImageView(image: foodShelfImage)
+        foodShelf.frame = CGRect(x: 107, y: 90, width: 808, height: 132)
+        
+        let panImage = UIImage(named: "Images/Interface/pan.png")
+        let pan = UIImageView(image: panImage)
+        pan.frame = CGRect(x: 363, y: 532, width: 297, height: 81)
+                
+        view.addSubview(background)
+        view.addSubview(foodShelf)
+        view.addSubview(pan)
+        self.view = view
+    }
 }
 
 let start = StartViewController()
 let cookbook = CookbookViewController()
+let kitchen = KitchenViewController()
 
 let navigation = UINavigationController(screenType: .ipad, isPortrait: true)
 navigation.pushViewController(start, animated: true)
 PlaygroundPage.current.liveView = navigation.scale(to: 0.5)
+
+/*:
+ 
+ # Receitas
+
+
+ 
+ ## Brigadeiro
+ _rende 30 porções_
+ 
+ ### Ingredientes
+ - 1 lata de leite condensado
+ - 2 colheres de sopa de manteiga
+ - 2 colheres de sopa de achocolatado em pó
+  
+ -----
+ 
+ ## Brownie de Chocolate
+ _rende 8 porções_
+ 
+ ### Ingredientes
+ - 1 barra de chocolate meio amargo (400g)
+ - 200g de manteiga
+ - 6 ovos
+ - 1 xícara e meia de açúcar
+ - 1 xícara e meia de farinha de trigo
+ 
+ -----
+ 
+ ## Torta de Morango
+ _rende 12 porções_
+ 
+ ### Ingredientes
+ - 1 xícara de farinha de trigo
+ - 1 gema de ovo
+ - 100g de manteiga
+ - 2 colheres de sopa de açúcar
+ - 1 colher de sopa de fermento em pó
+ 
+*/
--- CookingGame.playground/Sources/Classes/Cookbook.swift
+++ CookingGame.playground/Sources/Classes/Cookbook.swift
@@ -1,2 +1,19 @@
 import Foundation
 
+public class Cookbook {
+    var recipes: [Recipe]
+    var stars: [Int]
+    
+    init() {
+        // Ingredientes
+        let butter = Ingredient(name: "Manteiga", unitOfMeasurement: "gramas", imageSrc: "butter.png", availableSizes: [(40, "1 colher de sopa"), (100, "100 gramas")] )
+        let chocolatePowder = Ingredient(name: "Achocolatado em pó", unitOfMeasurement: "gramas", imageSrc: "chocolate_powder.png", availableSizes: [(5, "1 colher de sopa")])
+        let condensedMilk = Ingredient(name: "Leite condensado", unitOfMeasurement: "gramas", imageSrc: "condensed_milk.png", availableSizes: [(400, "1 lata"), (200, "1/2 lata")])
+        
+        // Receitas
+        let brigadeiro = Recipe(ingredients: [(condensedMilk, 200), (chocolatePowder, 5), (butter, 40)], servings: 15)
+        
+        recipes = [brigadeiro]
+        stars = [0]
+    }
+}
